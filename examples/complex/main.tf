3cde988b7190adec90176f826b548eee03f0c980
Kerim Satirli <k.satirli@synapticate.com>
2019-09-02 12:23:27 +0200

    adds `complex` example

---
+++ examples/complex/main.tf
@@ -0,0 +1,39 @@
+module "complex-example-target" {
+  source  = "operatehappy/s3-bucket/aws"
+  version = "1.0.0"
+  name    = "oh-demos-complex-example-target"
+  acl     = "log-delivery-write"
+
+  server_side_encryption_configuration = [{
+    kms_master_key_id = "alias/oh-demos"
+    sse_algorithm     = "aws:kms"
+  }]
+}
+
+module "complex-example" {
+  source  = "operatehappy/s3-bucket/aws"
+  version = "1.0.0"
+
+  name = "oh-demos-complex-example"
+
+  tags = {
+    Department = "Operations"
+  }
+
+  force_destroy = true
+
+  versioning = [{
+    enabled    = true
+    mfa_delete = false
+  }]
+
+  logging = [{
+    target_bucket = module.complex-example-target.id
+    target_prefix = "logs"
+  }]
+
+  server_side_encryption_configuration = [{
+    kms_master_key_id = "alias/oh-demos"
+    sse_algorithm     = "aws:kms"
+  }]
+}
